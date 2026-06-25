848a75f1856a70a13cc526e22772ffc90df7392d
crazyo786 <>
2026-06-25 07:41:23 +0000

    Challenge: Aggregate Monthly Sales

--- time_series_aggregation/challenge_aggregate_monthly_sales/query.sql
+++ time_series_aggregation/challenge_aggregate_monthly_sales/query.sql
@@ -1,1 +1,5 @@
 -- Write your code here
+SELECT DATE_TRUNC('month', transaction_date) :: date AS month, SUM(amount) AS total_sales
+FROM transactions
+    WHERE category = 'Sales'
+GROUP BY DATE_TRUNC ('month', transaction_date) :: date;
